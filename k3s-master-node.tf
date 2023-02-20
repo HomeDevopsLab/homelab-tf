6ed202d488c68561112105e73e22e869ca4ea3f4
Krzysztof Królikowski <kkrolikowski@gmail.com>
2023-02-20 21:37:38 +0100

    tf format fix

--- k3s-master-node.tf
+++ k3s-master-node.tf
@@ -1,8 +1,8 @@
 resource "null_resource" "k3s_master_node" {
   connection {
-    type = "ssh"
-    host = vars.master_node
-    user = var.vm_user
+    type     = "ssh"
+    host     = vars.master_node
+    user     = var.vm_user
     password = var.vm_pass
   }
   provisioner "remote-exec" {
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "master_node" {
   description = "K3S Master Node"
-  type = String
-  default = ["1.1.1.1"]
+  type        = String
+  default     = ["1.1.1.1"]
 }
 
 variable "vm_user" {
   description = "Generic VM login"
-  type = String
-  default = "generic"
+  type        = String
+  default     = "generic"
 }
 
 variable "vm_pass" {
   description = "Generic VM user password"
-  type = String
-  default = "secret"
+  type        = String
+  default     = "secret"
 }
