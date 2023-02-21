79645445d2c44a21a4dc73e7428091353abf910a
Krzysztof Królikowski <kkrolikowski@gmail.com>
2023-02-21 20:35:09 +0100

    update ssh opts

--- k3s-deploy-master-node.tf
+++ k3s-deploy-master-node.tf
@@ -17,7 +17,7 @@ data "external" "k3s_master_token" {
   depends_on = [
     null_resource.k3s_master_node
   ]
-  program = ["/bin/bash", "-c", "echo \"{\\\"token\\\":\\\"$(sshpass -p ${var.vm_pass} ssh ${var.vm_user}@${var.master_node} \"echo ${var.vm_pass} | sudo -S cat /var/lib/rancher/k3s/server/node-token\")\\\"}\""]
+  program = ["/bin/bash", "-c", "echo \"{\\\"token\\\":\\\"$(sshpass -p ${var.vm_pass} ssh -o StrictHostKeyChecking=no ${var.vm_user}@${var.master_node} \"echo ${var.vm_pass} | sudo -S cat /var/lib/rancher/k3s/server/node-token\")\\\"}\""]
 }
 
 output "k3s_master_token" {
