31c6f7f12ef14e0b4a6a3a539253f0d91058825c
Krzysztof Królikowski <kkrolikowski@gmail.com>
2023-02-21 20:17:43 +0100

    fix

--- k3s-deploy-master-node.tf
+++ k3s-deploy-master-node.tf
@@ -12,3 +12,15 @@ resource "null_resource" "k3s_master_node" {
     ]
   }
 }
+
+data "external" "k3s_master_token" {
+  depends_on = [
+    null_resource.k3s_master_node
+  ]
+  program = ["/bin/bash", "-c", "echo \"{\\\"token\\\":\\\"$(sshpass -p ${var.vm_pass} ssh ${var.vm_user}@${var.master_node} \"echo ${var.vm_pass} | sudo -S cat /var/lib/rancher/k3s/server/node-token\")\\\"}\""]
+}
+
+output "k3s_master_token" {
+  description = "K3S token needed for worker nodes"
+  value       = data.external.k3s_master_token.result.token
+}
