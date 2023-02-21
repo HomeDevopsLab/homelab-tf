73313b52eb0e473b804ff49e1edaedfbb213e262
Krzysztof Królikowski <kkrolikowski@gmail.com>
2023-02-21 01:03:25 +0100

    depedenecy

--- k3s-output-master-token.tf
+++ k3s-output-master-token.tf
@@ -1,4 +1,7 @@
 data "external" "k3s_master_token" {
+  depends_on = [
+    null_resource.k3s_master_node
+  ]
   program = ["/bin/bash", "-c", "echo \"{\\\"token\\\":\\\"$(sshpass -p ${var.vm_pass} ssh ${var.vm_user}@${var.master_node} \"echo ${var.vm_pass} | sudo -S cat /var/lib/rancher/k3s/server/node-token\")\\\"}\""]
 }
 
