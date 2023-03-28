89bf91c9f8bd82433d47094b61b766e8994aca8d
Krzysztof Królikowski <kkrolikowski@gmail.com>
2023-03-28 19:47:22 +0200

    update

--- k3s-save-local-config.tf
+++ k3s-save-local-config.tf
@@ -1,4 +1,4 @@
 resource "local_file" "k3s_config" {
-  content_base64 = data.external.k3s_kube_config
+  content_base64 = data.external.k3s_kube_config.kube_config
   filename       = "/tmp/foo.bar"
 }
