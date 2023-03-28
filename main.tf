f3c2098e5a7704d9271816be88bdf5bfe9dd97ca
Krzysztof Królikowski <kkrolikowski@gmail.com>
2023-03-28 22:47:55 +0200

    flux bootstrap setup

--- main.tf
+++ main.tf
@@ -12,6 +12,26 @@ terraform {
       source  = "hashicorp/local"
       version = "2.4.0"
     }
+    github = {
+      source  = "integrations/github"
+      version = ">= 4.5.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
+    flux = {
+      source = "fluxcd/flux"
+      version = "0.25.3"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
   }
   backend "local" {
     path = "/home/ubuntu/terraform/terraform.tfstate"
